689a8f98a8c1ddaaade07b27a26e60cd72232d92
noahfreriksen <noah_freriksen@hotmail.com>
2019-06-28 11:05:40 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,10 @@
+package com.company;
+
+import twitter4j.Twitter;
+
+public class Main {
+
+    public static void main(String[] args) {
+	// write your code here
+    }
+}
---
+++ src/com/company/Puller.java
@@ -0,0 +1,9 @@
+package com.company;
+
+public class Puller {
+
+    public Puller(){
+
+    }
+
+}
